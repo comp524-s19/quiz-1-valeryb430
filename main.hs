7d83cfe28f8a599ef791cbef3dff6e3aece2e381
Valery Briceno <valerybriceno@gmail.com>
2019-01-31 17:48:59 +0000

    quiz 1

--- main.hs
+++ main.hs
@@ -1,3 +1,3 @@
-grade (list 1, list2) = [a*b | (a,b) <- zip list1 list 2]
-gradew (list 1, list2) = sum (grade (list1,list2))
-finalGrade list1 list2 = gradew (list1, list2) `div` (sum list2)
+finalGrade :: [Int] -> [Int] -> Int
+finalGrade a, b = ((fst a * fst b) + finalGrade (drop 1 a), (drop 1 b)) `div` length a
+
